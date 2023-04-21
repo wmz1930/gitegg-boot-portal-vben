48fa8392c8020251a44a4dcba2d7165fe386efd0
jeebase <jeebase@jeebase.com>
2023-04-22 01:07:56 +0800

    修复msg

--- src/utils/http/axios/index.ts
+++ src/utils/http/axios/index.ts
@@ -53,13 +53,13 @@ const transform: AxiosTransform = {
       // return '[HTTP] Request has no return value';
       throw new Error(t('sys.api.apiRequestFailed'));
     }
-    //  这里 code，result，message为 后台统一的字段，需要在 types.ts内修改为项目自己的接口返回格式
-    const { code, result, message } = data;
+    //  这里 code，success，msg为 后台统一的字段，需要在 types.ts内修改为项目自己的接口返回格式
+    const { code, msg } = data;
 
     // 这里逻辑可以根据项目进行修改
     const hasSuccess = data && Reflect.has(data, 'code') && code === ResultEnum.SUCCESS;
     if (hasSuccess) {
-      let successMsg = message;
+      let successMsg = msg;
 
       if (isNull(successMsg) || isUnDef(successMsg) || isEmpty(successMsg)) {
         successMsg = t(`sys.api.operationSuccess`);
